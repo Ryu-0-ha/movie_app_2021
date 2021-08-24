d089f39b89490b2779c8c843528d752b93124b9b
Ryu-0-Ha <ryuyh2000@gmail.com>
2021-08-25 02:55:48 +0900

    02:55 끝

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import "./App.css";
+import Detail from "./routes/Detail";
 
-function App(){
-    return <span>something</span>
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail}/>
+    </BrowserRouter>
+  );
 }
 
 export default App;
